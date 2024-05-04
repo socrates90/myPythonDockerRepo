6b21796466e4017865063cab1069b204f98d3464
socrates90 <110427059+socrates90@users.noreply.github.com>
2024-05-03 20:02:05 -0500

    Deleted unneeded comment.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import random
 app = Flask(__name__)
 
 # list of cat images
-# this is another comment
 images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
